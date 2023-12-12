769c8b69f25ac7f8d198482751e8308f74e4e379
Ben Grogan <ben.grogan.2024@mumail.ie>
2023-12-12 14:10:15 +0000

    removed decclaring double early, declares on input

--- diceGame.java
+++ diceGame.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class diceGame {
     static Scanner scan = new Scanner(System.in);
 
-    {
+    public static void main(String[] args) {
         Q1();
         Q2();
         Q3();
@@ -23,24 +23,24 @@ public class diceGame {
             }
 
             if (input.equals("square")) {
-                double a;
+                // double a;
                 System.out.println("Enter the length of side a: ");
-                a = Double.parseDouble(scan.nextLine());
+                double a = Double.parseDouble(scan.nextLine());
                 System.out.println("The circumference of the square is: " + a * 4);
                 System.out.println("The area of the square is: " + a * a);
 
             } else if (input.equals("rectangle")) {
-                double a, b;
+                // double a, b;
                 System.out.println("Enter the length of side a: ");
-                a = Double.parseDouble(scan.nextLine());
+                double a = Double.parseDouble(scan.nextLine());
                 System.out.println("Enter the length of side b: ");
-                b = Double.parseDouble(scan.nextLine());
+                double b = Double.parseDouble(scan.nextLine());
                 System.out.println("The circumference of the rectangle is: " + (2 * a + 2 * b));
                 System.out.println("The area of the rectangle is: " + (a * b));
             } else if (input.equals("circle")) {
-                double r;
+                // double r;
                 System.out.println("Enter the radius: ");
-                r = Double.parseDouble(scan.nextLine());
+                double r = Double.parseDouble(scan.nextLine());
                 System.out.println("The circumference of the circle is: " + (Math.PI * r * 2));
                 System.out.println("The area of the circle is: " + (Math.PI * r * r));
             }
